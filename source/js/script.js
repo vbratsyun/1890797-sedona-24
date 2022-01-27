e005ce941d8ad4fe7035e8552ba072283fd75c29
vbratsyun <vbratsyun@yandex.ru>
2022-01-27 21:21:58 +0300

    исправления

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,15 @@
 
-var navMain = document.querySelector(".page-nav");
-var navToggle = document.querySelector(".page-nav__toggle");
+var navMain = document.querySelector('.page-nav');
+var navToggle = document.querySelector('.page-nav__toggle');
 
-navMain.classList.remove("page-nav--nojs");
+navMain.classList.remove('page-nav--nojs');
 
-navToggle.addEventListener("click", function () {
-  if (navMain.classList.contains("page-nav--closed")) {
-    navMain.classList.remove("page-nav--closed");
-    navMain.classList.add("page-nav--opened");
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('page-nav--closed')) {
+    navMain.classList.remove('page-nav--closed');
+    navMain.classList.add('page-nav--opened');
   } else {
-    navMain.classList.add("page-nav--closed");
-    navMain.classList.remove("page-nav--opened");
+    navMain.classList.add('page-nav--closed');
+    navMain.classList.remove('page-nav--opened');
   }
 });
